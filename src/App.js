caa5e648216f4373dd30b0d0e199f67859b7c265
langerno <langerno@msu.edu>
2023-04-20 21:28:42 -0400

    Added bootstrap, got search bar working

--- src/App.js
+++ src/App.js
@@ -1,40 +1,77 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
-function Pikachu(){
-  const [charm, setCharm] = useState([])
-  const [bulb, setBulb] = useState([])
-  const [squirt, setSquirt] = useState([])
+let currentPokeData = null
+let prevInput = null
 
-  const urls = ['https://pokeapi.co/api/v2/pokemon/charmander/', 
-                'https://pokeapi.co/api/v2/pokemon/bulbasaur/', 
-                'https://pokeapi.co/api/v2/pokemon/squirtle/',];
-
-  const getData = async () => {
-    const [result1, result2, result3] = await Promise.all(
-      urls.map((url) => fetch(url).then((res) => res.json()))
-    );
-    setCharm(result1);
-    setBulb(result2);
-    setSquirt(result3)
-  };
-
-  getData();
-
+function Search(){
   return (
+    <div class="mb-3">
+      <input id="searchBar" class="form-control" aria-describedby="emailHelp"/>
+    </div>
+  )
+
+}
+
+function SubmitButton(){
+  return(
     <>
-      <h1>
-        {charm.name} {bulb.name} {squirt.name}
-      </h1>
+      <button onClick={onSubmit}>Submit</button>
     </>
   )
 }
+
+async function onSubmit() {
+  let userInput = document.getElementById("searchBar").value.toLowerCase()
+  
+  // Adding this to avoiding spamming API calls
+  if (userInput !== prevInput) {
+    prevInput = userInput
+    let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + userInput +"/")
+    currentPokeData = await response.json();
+
+    console.log(currentPokeData)
+
+    if (currentPokeData !== null) {
+      document.getElementById("frontSprite").setAttribute("src", currentPokeData.sprites.front_default);
+      document.getElementById("frontSprite").setAttribute("alt", "Front Sprite of " + currentPokeData.name);
+
+      document.getElementById("backSprite").setAttribute("src", currentPokeData.sprites.back_default);
+      document.getElementById("backSprite").setAttribute("alt", "Back Sprite of " + currentPokeData.name);
+
+      document.getElementById("name").innerHTML = currentPokeData.name
+      document.getElementById("height").innerHTML = currentPokeData.height + " decimeters"
+    }
+  }
+}
+
+function PokeDesc(){
+  return(
+    <>
+      <h1 id="name">Welcome to your Pokedex!</h1>
+      <p id="height">Enter any pokemon name and click submit for data regarding it</p>
+    </>
+  )
+}
+
+function PokeImage() {
+  return(
+    <>
+      <img alt="" id="frontSprite"></img>
+      <img alt="" id="backSprite"></img>
+    </>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Pikachu />
+        <Search />
+        <SubmitButton />
+        <PokeImage />
+        <PokeDesc />
       </header>
     </div>
   );
