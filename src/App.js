66baa6e486aea7229cb7cc13381f674cb09feaaa
langerno <langerno@msu.edu>
2023-04-24 21:58:12 -0400

    More data pulled, tables

--- src/App.js
+++ src/App.js
@@ -4,21 +4,18 @@ import './App.css';
 let currentPokeData = null
 let prevInput = null
 
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 function Search(){
   return (
     <div class="mb-3">
       <input id="searchBar" class="form-control" aria-describedby="emailHelp"/>
+      <button onClick={onSubmit}>Submit</button>
     </div>
   )
 
-}
-
-function SubmitButton(){
-  return(
-    <>
-      <button onClick={onSubmit}>Submit</button>
-    </>
-  )
 }
 
 async function onSubmit() {
@@ -33,25 +30,38 @@ async function onSubmit() {
     console.log(currentPokeData)
 
     if (currentPokeData !== null) {
-      document.getElementById("frontSprite").setAttribute("src", currentPokeData.sprites.front_default);
-      document.getElementById("frontSprite").setAttribute("alt", "Front Sprite of " + currentPokeData.name);
-
-      document.getElementById("backSprite").setAttribute("src", currentPokeData.sprites.back_default);
-      document.getElementById("backSprite").setAttribute("alt", "Back Sprite of " + currentPokeData.name);
-
-      document.getElementById("name").innerHTML = currentPokeData.name
-      document.getElementById("height").innerHTML = currentPokeData.height + " decimeters"
+      updatePage();
     }
   }
 }
 
+
+
 function PokeDesc(){
   return(
     <>
-      <h1 id="name">Welcome to your Pokedex!</h1>
-      <p id="height">Enter any pokemon name and click submit for data regarding it</p>
+<table id="desc" class="table table-dark table-bordered table-sm">
+      <thead id="desc_head" class="thead-dark">
+        <th scope="col">Name</th>
+        <th scope="col">Height</th>
+        <th scope="col">Weight</th>
+      </thead>
+      <tbody id="desc_body">
+        <tr>
+          <td id="name"></td>
+          <td id="height"></td>
+          <td id="weight"></td>
+        </tr>
+      </tbody>
+    </table>
     </>
   )
+}
+
+function updateDesc(){
+  document.getElementById("name").innerHTML = capitalizeFirstLetter(currentPokeData.name)
+  document.getElementById("height").innerHTML = currentPokeData.height + " dm"
+  document.getElementById("weight").innerHTML = currentPokeData.weight + " hg"
 }
 
 function PokeImage() {
@@ -63,15 +73,84 @@ function PokeImage() {
   )
 }
 
+async function updateImage(){
+  document.getElementById("frontSprite").setAttribute("src", currentPokeData.sprites.front_default);
+  document.getElementById("frontSprite").setAttribute("alt", "Front Sprite of " + currentPokeData.name);
+
+  document.getElementById("backSprite").setAttribute("src", currentPokeData.sprites.back_default);
+  document.getElementById("backSprite").setAttribute("alt", "Back Sprite of " + currentPokeData.name);
+}
+
+
+
+function PokeAbilities(){
+  return(
+    <table id="abilities" class="table table-dark table-bordered table-sm">
+      <thead id="abilities_head" class="thead-dark">
+        <th scope="col">Name</th>
+        <th scope="col">Description</th>
+      </thead>
+      <tbody id="abilities_body">
+
+      </tbody>
+    </table>
+  )
+}
+
+async function updateAbilities() {
+  let ability_html = ""
+  console.log(currentPokeData.abilities)
+  for(let {ability} of currentPokeData.abilities) {
+    let response = await fetch(ability.url)
+    let ability_data = await response.json();
+    ability_html += "<tr scope='row'><td>" + capitalizeFirstLetter(ability.name) + "</td><td>" + ability_data.effect_entries[1].short_effect + "</td></tr>"
+  }
+  document.getElementById("abilities_body").innerHTML = ability_html
+}
+
+
+
+function PokeStats(){
+  return(
+    <table id="stats" class="table table-dark table-bordered table-sm">
+      <thead id="stats_head" class="thead-dark">
+        <th scope="col">Name</th>
+        <th scope="col">Base Count</th>
+        <th scope="col">Effort</th>
+      </thead>
+      <tbody id="stats_body">
+
+      </tbody>
+    </table>
+  )
+}
+
+async function updateStats() {
+  let stat_html = ""
+      console.log(currentPokeData.stats)
+      for(let stat of currentPokeData.stats) {
+        stat_html += "<tr scope='row'><td>" + capitalizeFirstLetter(stat.stat.name) + "</td><td>" + stat.base_stat + "</td><td>" + stat.effort + "</td></tr>"
+      }
+      document.getElementById("stats_body").innerHTML = stat_html
+}
+
+
+async function updatePage() {
+  updateAbilities()
+  updateStats()
+  updateImage()
+  updateDesc()
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Search />
-        <SubmitButton />
         <PokeImage />
         <PokeDesc />
+        <PokeAbilities />
+        <PokeStats />
       </header>
     </div>
   );
